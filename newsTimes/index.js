a3d48dbc8e7c7e411e6dc8c6424a96626e58ded4
YunheeAhn <ayh95944674@gmail.com>
2025-09-20 18:30:50 +0900

    오타 수정 및 가독성 위한 정리

--- newsTimes/index.js
+++ newsTimes/index.js
@@ -1,94 +1,91 @@
+//********** S : API 설정 **********//
 // API KEY 변수 저장
 const API_KEY = "96079dbac224485cb7775442e96e2c56";
-// News api (로컬확인용)
+
+// News api (로컬 확인용)
 // const newsAPI = "https://newsapi.org/v2/top-headlines?country=us";
+
 // Noona api (배포용)
 const newsAPI = "https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr";
 
-// 뉴스 전역변수로 선언
+// 뉴스 전역변수
 let newsList = [];
 
-// URL 전역변수로 선언
+// URL 전역변수
 let BASE_URL = new URL(`${newsAPI}&apiKey=${API_KEY}`);
+//********** E : API 설정 **********//
 
-// 뉴스 데이터 호출
+//********** S : 뉴스 데이터 호출 **********//
 const getNews = async () => {
-  // 에러핸들링 추가 250919
   try {
     // url 호출하기
     const response = await fetch(BASE_URL);
-    // json형식으로 data로 가져오기
     const data = await response.json();
 
-    // 200 ->api키 없을 때
+    // 상태 코드 확인
     if (response.status === 200) {
       if (data.articles.length === 0) {
-        // 검색 결과가 없을 때 : edge case
-        throw new Error("No result for this search");
+        throw new Error("No result for this search"); // 검색 결과 없음
       }
-      // Not Error
+
       // 뉴스 따로 저장
       newsList = data.articles;
       render();
     } else {
-      // Error situation
-      // 에러 메세지 던져주기
-      throw new Error(data.message);
+      throw new Error(data.message); // 에러 상황
     }
   } catch (error) {
-    // 에러메세지 유저 확인
-    errorRender(error.message);
+    errorRender(error.message); // 에러 메시지 출력
   }
 };
+//********** E : 뉴스 데이터 호출 **********//
 
-// 이미지가 없거나, 링크가 잘못 되었을 경우 대체 이미지
+//********** S : 기본 설정 **********//
+// 이미지 없거나 링크 잘못된 경우 대체 이미지
 const defaultImage =
   "https://img.freepik.com/premium-vector/green-start-button_875240-2897.jpg?w=1480";
 
-// 메뉴 가져오기
-// 메뉴에 클릭이벤트
+// 메뉴 가져오기 + 클릭 이벤트 등록
 const menus = document.querySelectorAll(".menus li a");
-menus.forEach((menu) => menu.addEventListener("click", (event) => getNewsCatagory(event)));
+menus.forEach((menu) => menu.addEventListener("click", (event) => getNewsCategory(event)));
 
-// 뉴스 정보 가져오기
+// 최신 뉴스 가져오기
 const getLatestNews = async () => {
-  // news api는 배포사이트에선 안보임, 로컬에서만 가능
-  // 상단 변수 분리로 선택 용이하게 수정
   BASE_URL = new URL(`${newsAPI}&apiKey=${API_KEY}`);
-
   getNews();
-
   console.log("뉴스생성", newsList);
 };
+//********** E : 기본 설정 **********//
 
 //********** S : 뉴스 내용 render **********//
 const render = () => {
   const newsHTML = newsList
     .map((news) => {
-      return `<div class="box">
-        <a target="_blank" href="${news.url}">
+      return `
+        <div class="box">
+          <a target="_blank" href="${news.url}">
             <div class="img_cont">
-                <img src="${news.urlToImage || defaultImage}" alt="${news.title}"
-     onerror="this.onerror=null; this.src='${defaultImage}';"/>
+              <img src="${news.urlToImage || defaultImage}" alt="${news.title}"
+                onerror="this.onerror=null; this.src='${defaultImage}';"/>
             </div>
             <div class="txt_cont">
-                <span class="cate">${news.category ? news.category : ""}</span>
-                <h2 class="title">${news.title}</h2>
-                <p class="cont">
-                  ${
-                    news.description == null || news.description == ""
-                      ? "내용없음"
-                      : news.description.length > 200
-                      ? news.description.slice(0, 200) + "..."
-                      : news.description
-                  }
-                </p>
-                <div class="source">
-                    <p class="company">${news.source.name ? news.source.name : "No Sources"}</p>
-                    <p class="date">${moment(news.publishedAt).fromNow()}</p>
-                </div>
+              <span class="cate">${news.category ? news.category : ""}</span>
+              <h2 class="title">${news.title}</h2>
+              <p class="cont">
+                ${
+                  news.description == null || news.description == ""
+                    ? "내용없음"
+                    : news.description.length > 200
+                    ? news.description.slice(0, 200) + "..."
+                    : news.description
+                }
+              </p>
+              <div class="source">
+                <p class="company">${news.source.name ? news.source.name : "No Sources"}</p>
+                <p class="date">${moment(news.publishedAt).fromNow()}</p>
+              </div>
             </div>
-        </a>
+          </a>
         </div>`;
     })
     .join("");
@@ -100,28 +97,26 @@ const render = () => {
 //********** S : 에러 메세지 render **********//
 const errorRender = (errorMessage) => {
   const errorHTML = `<div class="alert"><p>${errorMessage}</p></div>`;
-
   document.getElementById("news-board").innerHTML = errorHTML;
 };
 //********** E : 에러 메세지 render **********//
 
 //********** S : 검색 핸들링 **********//
 // 카테고리별 뉴스 가져오기
-const getNewsCatagory = async (event) => {
-  //  카테고리 읽어오기
+const getNewsCategory = async (event) => {
   const category = event.target.textContent.toLowerCase();
-
-  // 상단 변수 분리로 선택 용이하게 수정
   BASE_URL = new URL(`${newsAPI}&category=${category}&apiKey=${API_KEY}`);
-
   getNews();
 };
 
+// 검색창 요소
 const searchInput = document.getElementById("searchInput");
-// 인풋창 포커스시 입력 초기화
+
+// 포커스 시 입력 초기화
 function clearInput() {
   searchInput.value = "";
 }
+
 // 인풋창 엔터 입력
 searchInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
@@ -129,13 +124,10 @@ searchInput.addEventListener("keypress", function (event) {
   }
 });
 
+// 키워드 검색
 const getNewsByKeyword = async () => {
-  // search 인풋 값 가져오기
   const keyword = searchInput.value;
-
-  // 상단 변수 분리로 선택 용이하게 수정
   BASE_URL = new URL(`${newsAPI}&q=${keyword}&apiKey=${API_KEY}`);
-
   getNews();
 };
 //********** E : 검색 핸들링 **********//
@@ -146,12 +138,12 @@ const menuBtn = document.querySelector(".mobile_menu > .btn"); // 열기 버튼
 const menuInner = document.querySelector(".mobile_menu > .inner"); // 메뉴 영역
 const closeBtn = document.querySelector(".mobile_menu .close_btn"); // 닫기 버튼
 
-// 모바일메뉴 열기
+// 열기
 menuBtn.addEventListener("click", () => {
   menuInner.classList.add("show");
 });
 
-// 모바일메뉴 닫기
+// 닫기
 closeBtn.addEventListener("click", () => {
   menuInner.classList.remove("show");
 });
@@ -160,25 +152,25 @@ closeBtn.addEventListener("click", () => {
 const searchIcon = document.querySelector(".search_wrap > .search_icon");
 const inputWrap = document.querySelector(".search_wrap > .input_wrap");
 
-// 아이콘 클릭 시 input_wrap 토글
+// 검색 아이콘 클릭 시 input_wrap 토글
 searchIcon.addEventListener("click", () => {
   inputWrap.classList.toggle("show");
 });
 
-// gotoTop 버튼 이벤트
+// gotoTop 버튼
 const gotoTop = document.querySelector(".gotoTop");
 const topBtn = document.querySelector(".gotoTop .top");
-// 스크롤 이벤트로 버튼 보이기/숨기기
+
+// 스크롤 시 버튼 보이기/숨기기
 window.addEventListener("scroll", () => {
   if (window.scrollY > 200) {
-    // 스크롤이 200px 넘으면 보이기
     gotoTop.classList.add("show");
   } else {
     gotoTop.classList.remove("show");
   }
 });
 
-// 버튼 클릭 시 위로 스무스하게 이동
+// 버튼 클릭 시 위로 스무스 이동
 topBtn.addEventListener("click", (event) => {
   event.preventDefault();
   window.scrollTo({
@@ -186,6 +178,7 @@ topBtn.addEventListener("click", (event) => {
     behavior: "smooth",
   });
 });
-
 //********** E : 모바일 UI **********//
+
+//********** 실행 **********//
 getLatestNews();
